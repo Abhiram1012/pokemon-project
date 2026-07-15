afc310234107fd90bd130f70a08993dc03e92281
Abhiram <abhiramputta1210@gmail.com>
2026-07-15 17:50:15 +0530

    Initial Pokemon API program

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print("Pokemon:", data["name"])
+    print("Height:", data["height"])
+    print("Weight:", data["weight"])
+else:
+    print("API Failed")
